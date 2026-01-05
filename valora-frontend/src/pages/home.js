4811ce046727aac47e770cccb53c19f7d32038af
HA24RSH <harshvardhangadagade24555@gmail.com>
2026-01-05 15:41:54 +0530

    powered by section implemented

--- valora-frontend/src/pages/home.js
+++ valora-frontend/src/pages/home.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { PoweredBy } from '../components/poweredBy'
 
 export const Home = () => {
     return (
         <div className='container'>
-            <div style={{ padding: '2rem', textAlign: 'center' }}>
+            <div className='home-content'>
                 <h1>Welcome to Valora</h1>
                 <p>AI-Powered Interview Practice Platform</p>
+
+                <PoweredBy/>
+                
             </div>
         </div>
     )
